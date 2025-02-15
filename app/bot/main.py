04e6f52c59599aedbb880d9f83913505633f3593
Ринар199 <ibragimovrinat330@gmail.com>
2025-02-15 14:22:06 +0500

    local files

---
+++ app/bot/main.py
@@ -0,0 +1,39 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram.filters import Command
+from aiogram import Router
+
+bot = Bot('7105803545:AAEuXRFep1TdMyM5_UI5HjcOxA_K4D3bNig')
+dp = Dispatcher()
+
+router = Router()
+dp.include_router(router)
+
+@router.message(Command("start"))
+async def start(message):
+
+    button = KeyboardButton(
+        text='Открыть веб страницу',
+        web_app=WebAppInfo(url='https://example.com')
+    )
+    markup = ReplyKeyboardMarkup(
+        keyboard=[[button]],  # Клавиатура должна быть списком списков
+        resize_keyboard=True
+    )
+
+    await message.answer('Привет! Рады видеть вас!', reply_markup=markup)
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(dp.start_polling(bot))
+
+
+
+
+
+
+
+
+
+
+
