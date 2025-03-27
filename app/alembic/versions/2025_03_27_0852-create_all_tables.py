709edf9e0adbe4b8b626468168dba0f368c406aa
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2025-03-27 08:53:49 +0500

    upd: migration files

--- app/alembic/versions/2025_03_27_0852-create_all_tables.py
+++ app/alembic/versions/2025_03_27_0852-create_all_tables.py
@@ -1,8 +1,8 @@
 """create_all_tables
 
-Revision ID: fc05a9d8a1bd
+Revision ID: ef80b5d86d71
 Revises:
-Create Date: 2025-03-26 13:02:27.526070
+Create Date: 2025-03-27 08:52:39.557896
 
 """
 
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "fc05a9d8a1bd"
+revision: str = "ef80b5d86d71"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -23,6 +23,7 @@ def upgrade() -> None:
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("telegram_id", sa.String(), nullable=False),
         sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("telegram_id"),
     )
     op.create_table(
         "foods",
@@ -34,9 +35,37 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
     )
     op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("telegram_id", sa.String(), nullable=False),
+        sa.Column("first_name", sa.String(), nullable=False),
+        sa.Column("last_name", sa.String(), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_staff", sa.Boolean(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("telegram_id"),
+    )
+    op.create_table(
+        "basket_food",
+        sa.Column("basket_id", sa.Integer(), nullable=False),
+        sa.Column("food_id", sa.Integer(), nullable=False),
+        sa.Column("added_at", sa.DateTime(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["basket_id"],
+            ["baskets.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["food_id"],
+            ["foods.id"],
+        ),
+        sa.PrimaryKeyConstraint("basket_id", "food_id"),
+    )
+    op.create_table(
         "orders",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("telegram_id", sa.String(), nullable=False),
+        sa.Column("basket_id", sa.Integer(), nullable=False),
         sa.Column("address", sa.String(length=255), nullable=False),
         sa.Column("customer_name", sa.String(length=255), nullable=False),
         sa.Column("customer_phone", sa.String(length=20), nullable=False),
@@ -58,55 +87,17 @@ def upgrade() -> None:
         sa.Column("total_price", sa.DECIMAL(precision=10, scale=2), nullable=False),
         sa.Column("created_at", sa.DateTime(timezone=True), nullable=False),
         sa.Column("updated_at", sa.DateTime(timezone=True), nullable=False),
-        sa.PrimaryKeyConstraint("id"),
-    )
-    op.create_table(
-        "users",
-        sa.Column("id", sa.Integer(), nullable=False),
-        sa.Column("telegram_id", sa.String(), nullable=False),
-        sa.Column("first_name", sa.String(), nullable=False),
-        sa.Column("last_name", sa.String(), nullable=False),
-        sa.Column("is_active", sa.Boolean(), nullable=False),
-        sa.Column("is_staff", sa.Boolean(), nullable=False),
-        sa.Column("is_superuser", sa.Boolean(), nullable=False),
-        sa.PrimaryKeyConstraint("id"),
-        sa.UniqueConstraint("telegram_id"),
-    )
-    op.create_table(
-        "basket_food",
-        sa.Column("basket_id", sa.Integer(), nullable=False),
-        sa.Column("food_id", sa.Integer(), nullable=False),
-        sa.Column("quantity", sa.Integer(), nullable=False),
-        sa.Column("added_at", sa.DateTime(), nullable=False),
         sa.ForeignKeyConstraint(
             ["basket_id"],
             ["baskets.id"],
         ),
-        sa.ForeignKeyConstraint(
-            ["food_id"],
-            ["foods.id"],
-        ),
-        sa.PrimaryKeyConstraint("basket_id", "food_id"),
-    )
-    op.create_table(
-        "order_to_food",
-        sa.Column("order_id", sa.Integer(), nullable=True),
-        sa.Column("food_id", sa.Integer(), nullable=True),
-        sa.ForeignKeyConstraint(
-            ["food_id"],
-            ["foods.id"],
-        ),
-        sa.ForeignKeyConstraint(
-            ["order_id"],
-            ["orders.id"],
-        ),
+        sa.PrimaryKeyConstraint("id"),
     )
 
 
 def downgrade() -> None:
-    op.drop_table("order_to_food")
+    op.drop_table("orders")
     op.drop_table("basket_food")
     op.drop_table("users")
-    op.drop_table("orders")
     op.drop_table("foods")
     op.drop_table("baskets")
